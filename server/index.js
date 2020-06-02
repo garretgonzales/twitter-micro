e4a3813c4eb7811403510ceb6c5e9cc33889b64d
garretgonzales <garret915@gmail.com>
2020-06-02 16:40:21 -0500

    GET/POST working w/MongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const monk = require('monk');
 
 const app = express();
+
+const db = monk('localhost/tweeter');
+const tweets = db.get('tweets'); // collection within db - created automatically by mongo
+
+
+
 app.use(cors());
 app.use(express.json()); // bodyparser built into express middleware
 
@@ -11,6 +18,13 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/tweets', (req, res) => {
+    tweets
+        .find()
+        .then(tweets => {
+            res.json(tweets);
+        });
+});
 
 function isValidTweet(tweet) {
     return tweet.name && tweet.name.toString().trim() !== '' &&
@@ -22,9 +36,15 @@ app.post('/tweets', (req, res) => {
         // insert into db..
         const tweet = {
             name: req.body.name.toString(),
-            content: req.body.content.toString()
+            content: req.body.content.toString(),
+            created: new Date()
         };
 
+        tweets
+            .insert(tweet)
+            .then(createdTweet => {
+                res.json(createdTweet);
+            });
         //console.log(tweet);
     } else {
         res.status(422);
