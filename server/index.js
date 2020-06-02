2cafaa570156e0ae163a467d78262bde926ec6a2
garretgonzales <garret915@gmail.com>
2020-06-02 16:14:32 -0500

    Fixed non-existent bug. 100% functional

--- server/index.js
+++ server/index.js
@@ -18,20 +18,21 @@ function isValidTweet(tweet) {
 }
 
 app.post('/tweets', (req, res) => {
-    console.log(req.body);
     if (isValidTweet(req.body)) {
         // insert into db..
         const tweet = {
             name: req.body.name.toString(),
             content: req.body.content.toString()
         };
+
+        //console.log(tweet);
     } else {
         res.status(422);
         res.json({
             message: 'Fill in all fields.'
         });
     }
-})
+});
 
 app.listen(5001, () => {
     console.log('Listening on http://localhost:5001');
