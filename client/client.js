4e7e43483cdf164188fa0839c64cf8a9f9cf2d9a
garretgonzales <garret915@gmail.com>
2020-06-02 17:02:13 -0500

    Loading All Tweets :fire:

--- client/client.js
+++ client/client.js
@@ -1,10 +1,16 @@
+// import {
+//     response
+// } from "express";
+
 const form = document.querySelector('form');
-
 const loadingSpinner = document.querySelector('.loading');
-
 const API_URL = 'http://localhost:5001/tweets';
+const tweetElement = document.querySelector('.tweets');
 
-loadingSpinner.style.display = 'none';
+
+loadingSpinner.style.display = '';
+
+listAllTweets();
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -23,10 +29,44 @@ form.addEventListener('submit', (event) => {
     form.style.display = 'none';
 
     fetch(API_URL, {
-        method: 'POST',
-        body: JSON.stringify(tweet),
-        headers: {
-            'content-type': 'application/json'
-        }
-    });
+            method: 'POST',
+            body: JSON.stringify(tweet),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(response => response.json())
+        .then(createdTweet => {
+            form.reset();
+            form.style.display = '';
+            listAllTweets();
+        });
 });
+
+function listAllTweets() {
+    tweetElement.innerHTML = '';
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(tweets => {
+            console.log(tweets);
+            tweets.reverse();
+            tweets.forEach(tweet => {
+                const div = document.createElement('div');
+
+                const header = document.createElement('h3');
+                header.textContent = tweet.name;
+
+                const contents = document.createElement('p');
+                contents.textContent = tweet.content;
+
+                const date = document.createElement('small');
+                date.textContent = new Date(tweet.created);
+
+                div.appendChild(header);
+                div.appendChild(contents);
+                div.appendChild(date);
+
+                tweetElement.appendChild(div);
+            });
+            loadingSpinner.style.display = 'none';
+        });
+}
